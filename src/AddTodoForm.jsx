7b9387adf18ea1e874b1cfacfcfd6247b6509e3b
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-06-25 20:09:05 -0700

    week 2 todolist

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,11 @@
+const AddTodoForm = () => {
+    return (
+        <form>
+            <label htmlFor='todoTitle'>Title: </label>
+            <input id='todoTitle'></input>
+            <button type='submit'>Add</button>
+        </form>
+    )
+}
+
+export default AddTodoForm; 
